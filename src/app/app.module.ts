2655eb51cd6000e9fd46d240b247e75f1280f051
JSanLazaro <jsanlazaro@hotmail.com>
2023-06-26 14:20:47 +0200

    merged favorites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,6 @@ import { MainComponent } from './shared/main/main.component';
 import { MaincardComponent } from './components/maincard/maincard.component';
 import { ListComponent } from './components/characters/list/list.component';
 import { ModelComponent } from './model/model.component';
-<<<<<<< HEAD
-=======
-import { HeartComponent } from './heart/heart.component';
-import { FavoriteratingComponent } from './components/favoriterating/favoriterating.component';
->>>>>>> 3bd2fff1d5b3a5ad749f47275b9b71d79d5e8418
 
 import { HeartComponent } from './heart/heart.component';
 import { FavListComponent } from './components/characters/fav-list/fav-list.component';
@@ -30,7 +25,6 @@ import { FavCardComponent } from './components/fav-card/fav-card.component'
     MainComponent,
     ModelComponent,
     HeartComponent,
-<<<<<<< HEAD
     FavListComponent,
     ListComponent,
     MaincardComponent,
@@ -38,9 +32,6 @@ import { FavCardComponent } from './components/fav-card/fav-card.component'
     CircleCharacterComponent,
     FavCardComponent
     
-=======
-    FavoriteratingComponent
->>>>>>> 3bd2fff1d5b3a5ad749f47275b9b71d79d5e8418
   ],
   imports: [
     BrowserModule,
--- src/app/components/maincard/maincard.component.ts
+++ src/app/components/maincard/maincard.component.ts
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { Component,ElementRef,Input,ViewChild } from '@angular/core';
-import { Character } from 'src/app/model/Character.model';
-import { SharedService } from 'src/app/services/shared.service';
-import { Renderer2 } from '@angular/core';
-import { SidebarComponent } from '../sidebar/sidebar.component';
-=======
 import { HtmlParser } from '@angular/compiler';
 import {
   Component,
@@ -19,7 +12,6 @@ import {
 } from '@angular/core';
 import { Character } from 'src/app/model/Character.model';
 import { MaincardturnComponent } from 'src/app/components/maincardturn/maincardturn.component';
->>>>>>> 3bd2fff1d5b3a5ad749f47275b9b71d79d5e8418
 
 @Component({
   selector: 'app-maincard',
@@ -30,44 +22,6 @@ import { MaincardturnComponent } from 'src/app/components/maincardturn/maincardt
   ],
 })
 export class MaincardComponent {
-<<<<<<< HEAD
-  @Input() character!:Character;
-  @ViewChild('card') cardDiv!: ElementRef;
-  texto:string;
-  isFront:boolean;
-  constructor(private sharedService: SharedService,private renderer: Renderer2){
-    this.texto = "";
-    this.isFront = true;
-  }
-  
-
-  addToFavorites(){
-    console.log("added to favorites "+ this.character._id);
-    this.sharedService.favoritesObject.addId(this.character._id);
-    this.sharedService.toggleOpen();
-  }
-  removeFromFavoties(id:number){
-    console.log("removed to favorites "+ this.character._id);
-    this.sharedService.favoritesObject.removeId(id);
-  }
-  turn(){
-    if(this.isFront){
-      this.addTurnClass;
-    }else{
-      this.addReTurnClass;
-    }
-    this.isFront = !this.isFront;    
-  }
-  addTurnClass(){
-    //this.myButton.nativeElement.classList.add("my-class"); //BAD PRACTICE
-    this.renderer.removeClass(this.cardDiv.nativeElement, "re-turn")
-    this.renderer.addClass(this.cardDiv.nativeElement, "turn");    
-  }
-  addReTurnClass(){
-    //this.myButton.nativeElement.classList.add("my-class"); //BAD PRACTICE
-    this.renderer.removeClass(this.cardDiv.nativeElement, "turn")
-    this.renderer.addClass(this.cardDiv.nativeElement, "re-turn");    
-=======
   @Input() character!: Character;
   @ViewChild('card') cardDiv!: ElementRef;
 
@@ -97,6 +51,5 @@ export class MaincardComponent {
       this.removeTurnClass();
     }
     this.isFront = !this.isFront;
->>>>>>> 3bd2fff1d5b3a5ad749f47275b9b71d79d5e8418
   }
 }
